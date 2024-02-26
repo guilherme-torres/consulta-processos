65f9db4faa9fce757539aa06721eaae94177d2c6
guilherme-torres <guilhermetorres1401@gmail.com>
2024-02-26 10:38:29 -0300

    validar schema xml

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from zeep import Client
 from dotenv import load_dotenv
 import os
+from helpers import consultar_processo
 
 load_dotenv()
 
 ID_CONSULTANTE = os.environ['ID_CONSULTANTE']
 SENHA_CONSULTANTE = os.environ['SENHA_CONSULTANTE']
-
-def consultar_processo(id_consultante, senha_consultante, numero_processo, grau=1):
-    wsdl = f'https://pje.tjpi.jus.br/{grau}g/intercomunicacao?wsdl'
-    client = Client(wsdl=wsdl)
-
-    with client.settings(raw_response=True, strict=False):
-        response = client.service.consultarProcesso(
-            id_consultante, senha_consultante, numero_processo
-        )
-        content = response.content
-    return content
-
 
 numero_processo = '1234'
 print(consultar_processo(ID_CONSULTANTE, SENHA_CONSULTANTE, numero_processo))
---
+++ helpers.py
@@ -0,0 +1,31 @@
+from lxml import etree
+from zeep import Client
+
+def extrair_xml(response_data):
+    start_index = response_data.find('<soap:Envelope')
+    stop = '</soap:Envelope>'
+    stop_index = response_data.find(stop) + len(stop)
+    xml = response_data[start_index:stop_index]
+    return xml
+
+
+def validar_xml(xml):
+    tree = etree.fromstring(xml)
+    schema = etree.XMLSchema(file='schema.xsd')
+    return schema.validate(tree)
+
+
+def consultar_processo(id_consultante, senha_consultante, numero_processo, grau=1):
+    wsdl = f'https://pje.tjpi.jus.br/{grau}g/intercomunicacao?wsdl'
+    client = Client(wsdl=wsdl)
+
+    with client.settings(raw_response=True, strict=False):
+        response = client.service.consultarProcesso(
+            id_consultante, senha_consultante, numero_processo
+        )
+        content = extrair_xml(str(response.content))
+
+    if not validar_xml(content):
+        return False
+    
+    return content
