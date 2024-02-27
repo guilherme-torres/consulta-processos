7eb0638ed163483ad8afcb35f5af1c34ca373188
guilherme-torres <guilhermetorres1401@gmail.com>
2024-02-27 09:56:00 -0300

    criar classe ConsultaProcesso

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ load_dotenv()
 ID_CONSULTANTE = os.environ['ID_CONSULTANTE']
 SENHA_CONSULTANTE = os.environ['SENHA_CONSULTANTE']
 
-numero_processo = '0816547-72.2023.8.18.0140'
+numero_processo = '1234'
 ConsultaProcesso(ID_CONSULTANTE, SENHA_CONSULTANTE, numero_processo).dados_basicos()
